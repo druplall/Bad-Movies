7df80860b7560ab0d6e51b791b9f5f68bf0b13e8
Deo-Mac <druplall@gmail.com>
2020-07-29 15:13:26 -0400

    Baseline code

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   user: 'root',
   password: '',
   database: 'badmovies',
-  API_KEY: '',
+  API_KEY: `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZWJiMmFjMTc2NTQzNzA4ODBhYjM3YjgxNTNlYjFlMyIsInN1YiI6IjVmMjBiNjczNWIyZjQ3MDAzNjRhYWRjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rsGsQbHRQ34FS8OJI5ZaW5x-3i_CHwSLbHXwLn7j9bo`,
 };
